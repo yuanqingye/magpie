01b54010ef8e79b58f081f58ece028b1caf0e6f4
yuanqingye <yuan.qingye@yahoo.com>
2020-05-26 19:22:00 +0800

    change adapt to chinese
    a change adapt to chinese jieba and word-sep

--- magpie/base/document.py
+++ magpie/base/document.py
@@ -1,20 +1,25 @@
 from __future__ import print_function, unicode_literals
 
+import re
 import io
 import os
 import nltk
 import string
+import jieba
 
 from nltk.tokenize import WordPunctTokenizer, sent_tokenize, word_tokenize
 
 nltk.download('punkt', quiet=True)  # make sure it's downloaded before using
 
 class Document(object):
+
     """ Class representing a document that the keywords are extracted from """
     def __init__(self, doc_id, filepath, text=None):
         self.doc_id = doc_id
 
         if text:
+            text = self.clean_text(text)
+            text = self.seg_text(text)
             self.text = text
             self.filename = None
             self.filepath = None
@@ -24,11 +29,33 @@ class Document(object):
 
             self.filepath = filepath
             self.filename = os.path.basename(filepath)
-
-            with io.open(filepath, 'r', encoding='utf-8') as f:
-                self.text = f.read()
-
+            with io.open(filepath, 'r', encoding='gbk') as f:
+                text_context = f.read()
+                text_context = self.clean_text(text_context)
+                self.text = self.seg_text(text_context)
+                print(self.text)
         self.wordset = self.compute_wordset()
+
+
+
+    # 利用jieba包进行分词，并并且去掉停词，返回分词后的文本
+    def seg_text(self,text):
+        stop = [line.strip() for line in open('data/stopwords.txt', encoding='utf8').readlines()]
+        text_seged = jieba.cut(text.strip())
+        outstr = ''
+        for word in text_seged:
+            if word not in stop:
+                outstr += word
+                outstr += ""
+        return outstr.strip()
+
+    # 清洗文本，去除标点符号数字以及特殊符号
+    def clean_text(self,content):
+        text = re.sub(r'[+——！，；／·。？、~@#￥%……&*“”《》：（）［］【】〔〕]+', '', content)
+        text = re.sub(r'[▲!"#$%&\'()*+,-./:;<=>\\?@[\\]^_`{|}~]+', '', text)
+        text = re.sub('\d+', '', text)
+        text = re.sub('\s+', '', text)
+        return text
 
     def __str__(self):
         return self.text
